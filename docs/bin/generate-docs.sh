08ed5c6c3000f119ca70ce600ce4a5e10908c7cf
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2021-01-25 11:45:42 +0100

    Fixing building docs for both SDD and ICD. Improving ICD

--- docs/bin/generate-docs.sh
+++ docs/bin/generate-docs.sh
@@ -10,23 +10,21 @@ trap "cd '${ORIG_DIR}'" EXIT
 cd "${BIN_DIR}/.."
 
 # Set context dependant on whether this script has been invoked by Travis or not
-if [ "${TRAVIS}" = "true" ]
-then
-  echo "Running in Travis..."
-else
-  # Running locally we want to remove the docker containers
-  export DOCKER_RM="--rm"
-fi
 
-# Prepare output/ directory
-rm -rf output
-mkdir -p output
-cp -r images output
-cp -r stylesheets output
+for doc in SDD ICD; do
+  cd $doc
+  # Prepare output/ directory
+  rm -rf output
+  mkdir -p output
+  cp -r images output
+  cp -r stylesheets output
 
-# Docuemnt Generation - using asciidoctor docker image
-#
-# HTML version
-docker run ${DOCKER_RM} -v $PWD:/documents/ --name asciidoc-to-html asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram -D /documents/output index.adoc
-# PDF version
-docker run ${DOCKER_RM} -v $PWD:/documents/ --name asciidoc-to-pdf asciidoctor/docker-asciidoctor asciidoctor-pdf -r asciidoctor-diagram -D /documents/output index.adoc
+  # Docuemnt Generation - using asciidoctor docker image
+  #
+  # HTML version
+  docker run --rm -v $PWD:/documents/ --name asciidoc-to-html asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram -D /documents/output index.adoc
+  # PDF version
+  docker run --rm -v $PWD:/documents/ --name asciidoc-to-pdf asciidoctor/docker-asciidoctor asciidoctor-pdf -r asciidoctor-diagram -D /documents/output index.adoc
+
+  cd -
+done
