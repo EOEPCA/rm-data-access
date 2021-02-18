9be6232de3f178c808ead8aff2dc2e0b3064caeb
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2021-02-18 17:17:50 +0100

    Adding startup script to init collection metadata

---
+++ core/registrar_pycsw/initialize-collections.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+python3 -c "
+from registrar.backend import get_backends
+from registrar.config import load_config
+
+config = load_config('/config.yaml')
+for backend in get_backends(config, ''):
+    if isinstance(backend, PycswBackend):
+        backend.load_collection_level_metadata()
+"
