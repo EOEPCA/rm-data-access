00d395beabeaa8a5d627061718f1b4930d93f581
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2021-02-19 09:18:09 +0100

    Adding debug output when loading collection level metadata

--- core/registrar_pycsw/initialize-collections.sh
+++ core/registrar_pycsw/initialize-collections.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 python3 -c "
 from registrar.backend import get_backends
 from registrar.config import load_config
@@ -7,5 +8,6 @@ from registrar.config import load_config
 config = load_config('/config.yaml')
 for backend in get_backends(config, ''):
     if isinstance(backend, PycswBackend):
+        print('Loading collection metadata')
         backend.load_collection_level_metadata()
 "
