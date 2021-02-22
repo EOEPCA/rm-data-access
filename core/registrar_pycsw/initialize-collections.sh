2de5f6171a4803d627a7b3e1f9f178dd8c92fba6
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2021-02-22 12:06:01 +0100

    Fixing script to load collection level metadata

--- core/registrar_pycsw/initialize-collections.sh
+++ core/registrar_pycsw/initialize-collections.sh
@@ -4,8 +4,9 @@
 python3 -c "
 from registrar.backend import get_backends
 from registrar.config import load_config
+from registrar_pycsw.backend import PycswBackend
 
-config = load_config('/config.yaml')
+config = load_config(open('/config.yaml'))
 for backend in get_backends(config, ''):
     if isinstance(backend, PycswBackend):
         print('Loading collection metadata')
