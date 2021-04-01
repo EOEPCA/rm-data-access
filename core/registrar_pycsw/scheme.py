8cffdfa370025ac0cc5d13893e53d90ccb877c47
baloola <baloola-mu@hotmail.com>
2021-04-01 17:24:12 +0200

    initial cwl scheme

---
+++ core/registrar_pycsw/scheme.py
@@ -0,0 +1,36 @@
+import re
+import logging
+import yaml
+from typing import List
+
+from registrar.source import Source
+from registrar.context import Context
+
+logger = logging.getLogger(__name__)
+
+
+class RegistrationScheme:
+    def get_context(self, source: Source, path: str) -> List[Context]:
+        raise NotImplementedError
+
+
+class CWLRegistrationScheme(RegistrationScheme):
+    name = 'cwl'
+
+    def __init__(self, level_re: str = r'.*(Level_[0-9]+)$'):
+        self.level_re = level_re
+
+    def get_context(self, source: Source, path: str) -> List[Context]:
+
+        cwl_filenames = source.list_files(path, ['CWL*.cwl', 'CWL*.CWL'])
+        cwl_file = cwl_filenames[0]
+
+        cwl = yaml.load(cwl_file, Loader=yaml.SafeLoader)
+
+        wf = list(filter(lambda x: x['class'] == 'Workflow', cwl['$graph']))[0]
+
+        return [Context(
+            identifier=wf['id'],
+            path=path,
+            scheme=self.name
+        )]
