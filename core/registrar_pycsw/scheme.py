17394885d24b7e6ab8dbb86f0f62a2c9059937a4
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2021-04-12 18:22:40 +0200

    Fixing reading of CWL yaml

--- core/registrar_pycsw/scheme.py
+++ core/registrar_pycsw/scheme.py
@@ -1,7 +1,9 @@
-import re
+from os import remove
+from os.path import join, basename
 import logging
 import yaml
 from typing import List
+from tempfile import gettempdir
 
 from registrar.source import Source
 from registrar.context import Context
@@ -10,14 +12,25 @@ from registrar.scheme import RegistrationScheme
 logger = logging.getLogger(__name__)
 
 
+def read_yaml(source: Source, path: str) -> Union[Dict, List]:
+    """ Helper to easily read a file on a source as JSON and decode it.
+    """
+    out_filename = join(gettempdir(), basename(path))
+    try:
+        source.get_file(path, out_filename)
+        with open(out_filename) as f:
+            data = yaml.load(f, Loader=yaml.SafeLoader)
+    finally:
+        remove(out_filename)
+    return data
+
+
 class CWLRegistrationScheme(RegistrationScheme):
     name = 'cwl'
 
     def get_context(self, source: Source, path: str) -> List[Context]:
         cwl_filenames = source.list_files(path, ['*.cwl', '*.CWL'])
-
-        with open(cwl_filenames[0]) as cwl_file:
-            cwl = yaml.load(cwl_file, Loader=yaml.SafeLoader)
+        cwl = read_yaml(source, cwl_filenames[0])
 
         workflow = next(iter([
             graph_item
