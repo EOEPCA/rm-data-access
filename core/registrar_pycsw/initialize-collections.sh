38088ef695347313c630362a5fea299221afd095
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2021-07-14 15:23:58 +0300

    Fixing registration of collection level metadata

--- core/registrar_pycsw/initialize-collections.sh
+++ core/registrar_pycsw/initialize-collections.sh
@@ -5,9 +5,14 @@ python3 -c "
 from registrar.backend import get_backends
 from registrar.config import load_config
 from registrar_pycsw.backend import PycswBackend
+from registrar.context import Context
 
 config = load_config(open('/config.yaml'))
-for backend in get_backends(config, ''):
+
+context = Context()
+context.schema = context.path = None
+
+for backend in get_backends(config, context):
     if isinstance(backend, PycswBackend):
         print('Loading collection metadata')
         backend.load_collection_level_metadata()
