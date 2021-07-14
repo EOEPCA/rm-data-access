c42e5fd2720c9d2c075a0893cdbf04348717aea0
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2021-07-14 17:18:31 +0300

    Fixing context again

--- core/registrar_pycsw/initialize-collections.sh
+++ core/registrar_pycsw/initialize-collections.sh
@@ -9,8 +9,7 @@ from registrar.context import Context
 
 config = load_config(open('/config.yaml'))
 
-context = Context(identifier='None', path='None', scheme='None')
-context.schema = context.path = None
+context = Context(identifier='dummy', path='dummy', scheme='dummy')
 
 for backend in get_backends(config, context):
     if isinstance(backend, PycswBackend):
