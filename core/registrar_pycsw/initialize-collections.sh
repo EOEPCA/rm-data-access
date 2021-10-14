14fe763479e1733620a90f721c0a27e3a4305976
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2021-10-14 22:46:15 +0200

    Updating for new version

--- core/registrar_pycsw/initialize-collections.sh
+++ core/registrar_pycsw/initialize-collections.sh
@@ -5,13 +5,10 @@ python3 -c "
 from registrar.backend import get_backends
 from registrar.config import load_config
 from registrar_pycsw.backend import PycswBackend
-from registrar.context import Context
 
 config = load_config(open('/config.yaml'))
 
-context = Context(identifier='dummy', path='dummy', scheme='dummy')
-
-for backend in get_backends(config, context):
+for backend in get_backends(config):
     if isinstance(backend, PycswBackend):
         print('Loading collection metadata')
         backend.load_collection_level_metadata()
--- core/registrar_pycsw/scheme.py
+++ core/registrar_pycsw/scheme.py
@@ -1,45 +1,45 @@
-from os import remove
-from os.path import join, basename
-import logging
-import yaml
-from typing import List, Union, Dict
-from tempfile import gettempdir
+# from os import remove
+# from os.path import join, basename
+# import logging
+# import yaml
+# from typing import List, Union, Dict
+# from tempfile import gettempdir
 
-from registrar.source import Source
-from registrar.context import Context
-from registrar.scheme import RegistrationScheme
+# from registrar.source import Source
+# from registrar.context import Context
+# from registrar.scheme import RegistrationScheme
 
-logger = logging.getLogger(__name__)
-
-
-def read_yaml(source: Source, path: str) -> Union[Dict, List]:
-    """ Helper to easily read a file on a source as JSON and decode it.
-    """
-    out_filename = join(gettempdir(), basename(path))
-    try:
-        source.get_file(path, out_filename)
-        with open(out_filename) as f:
-            data = yaml.load(f, Loader=yaml.SafeLoader)
-    finally:
-        remove(out_filename)
-    return data
+# logger = logging.getLogger(__name__)
 
 
-class CWLRegistrationScheme(RegistrationScheme):
-    name = 'cwl'
+# def read_yaml(source: Source, path: str) -> Union[Dict, List]:
+#     """ Helper to easily read a file on a source as JSON and decode it.
+#     """
+#     out_filename = join(gettempdir(), basename(path))
+#     try:
+#         source.get_file(path, out_filename)
+#         with open(out_filename) as f:
+#             data = yaml.load(f, Loader=yaml.SafeLoader)
+#     finally:
+#         remove(out_filename)
+#     return data
 
-    def get_context(self, source: Source, path: str) -> List[Context]:
-        cwl_filenames = source.list_files(path, ['*.cwl', '*.CWL'])
-        cwl = read_yaml(source, cwl_filenames[0])
 
-        workflow = next(iter([
-            graph_item
-            for graph_item in cwl['$graph']
-            if graph_item['class'] == 'Workflow'
-        ]), None)
+# class CWLRegistrationScheme(RegistrationScheme):
+#     name = 'cwl'
 
-        return [Context(
-            identifier=workflow['id'],
-            path=cwl_filenames[0],
-            scheme=self.name
-        )]
+#     def get_context(self, source: Source, path: str) -> List[Context]:
+#         cwl_filenames = source.list_files(path, ['*.cwl', '*.CWL'])
+#         cwl = read_yaml(source, cwl_filenames[0])
+
+#         workflow = next(iter([
+#             graph_item
+#             for graph_item in cwl['$graph']
+#             if graph_item['class'] == 'Workflow'
+#         ]), None)
+
+#         return [Context(
+#             identifier=workflow['id'],
+#             path=cwl_filenames[0],
+#             scheme=self.name
+#         )]
