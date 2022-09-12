2a3d6ffa20b743b35756693663e680ee6a2796b6
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2022-09-12 20:47:22 +0200

    Fixing `initialize-collections.sh` for new registration route setup

--- core/registrar_pycsw/initialize-collections.sh
+++ core/registrar_pycsw/initialize-collections.sh
@@ -3,12 +3,12 @@
 
 python3 -c "
 from registrar.backend import get_backends
-from registrar.config import load_config
+from registrar.config import RegistrarConfig
 from registrar_pycsw.backend import PycswMixIn, ItemBackend
 
-config = load_config(open('/config.yaml'))
+config = RegistrarConfig.from_file(open('/config.yaml'))
 
-for backend in get_backends(config):
+for backend in get_backends(config.routes['items'].backends):
     if isinstance(backend, ItemBackend):
         print('Loading collection metadata')
         backend.load_collection_level_metadata()
