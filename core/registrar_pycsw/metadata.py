967e6841d21d6f7bc5a6c09a19b6c6e09e77c346
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2023-03-07 16:54:05 +0200

    Use base_url to make sure it is handled by the class constructor

--- core/registrar_pycsw/metadata.py
+++ core/registrar_pycsw/metadata.py
@@ -763,12 +763,12 @@ class ISOMetadata:
 
         return iso_os.write(mcf)
 
-    def from_csw(self, csw_url: str, capabilities) -> str:
+    def from_csw(self, capabilities) -> str:
         mcf = deepcopy(self.mcf)
 
         now = datetime.now().isoformat()
 
-        csw_id = re.sub('[^a-zA-Z0-9 \n]', '-', csw_url)
+        csw_id = re.sub('[^a-zA-Z0-9 \n]', '-', self.base_url)
         mcf['metadata']['identifier'] = csw_id
         mcf['metadata']['hierarchylevel'] = 'service'
         mcf['metadata']['datestamp'] = now
@@ -789,7 +789,7 @@ class ISOMetadata:
 
         mcf['distribution']['http'] = {
             'rel': 'service',
-            'url': csw_url,
+            'url': self.base_url,
             'type': 'application/xml',
             'name': capabilities.identification.title,
             'description': capabilities.identification.abstract,
